8505649441d4b6c70f98a850bf533e46472fdd65
Lados597 <bykingg795@gmail.com>
2025-04-11 08:39:02 +0300

    Add files via upload

--- Lab_1.3.cpp
+++ Lab_1.3.cpp
@@ -14,40 +14,38 @@ protected:
 
 public:
     Entity(const std::string& n, int h, int a, int d)
-        : name(n), health(h), attackPower(a), defense(d) {
-    }
+        : name(n), health(h), attackPower(a), defense(d) {}
 
     virtual ~Entity() {}
 
-    // Виртуальный метод атаки
+    
     virtual void attack(Entity& target) {
         int damage = attackPower - target.getDefense();
         if (damage > 0) {
             target.takeDamage(damage);
-            std::cout << name << " attacks " << target.getName()
-                << " for " << damage << " damage!" << std::endl;
-        }
-        else {
-            std::cout << name << "'s attack has no effect on "
-                << target.getName() << "!" << std::endl;
+            std::cout << name << " attacks " << target.getName() 
+                      << " for " << damage << " damage!" << std::endl;
+        } else {
+            std::cout << name << "'s attack has no effect on " 
+                      << target.getName() << "!" << std::endl;
         }
     }
 
-    // Виртуальный метод лечения
+    
     virtual void heal(int amount) {
         health += amount;
-        std::cout << name << " heals for " << amount
-            << " HP. Current HP: " << health << std::endl;
+        std::cout << name << " heals for " << amount 
+                  << " HP. Current HP: " << health << std::endl;
     }
 
-    // Виртуальный метод отображения информации
+    
     virtual void displayInfo() const {
-        std::cout << "Name: " << name << ", HP: " << health
-            << ", Attack: " << attackPower
-            << ", Defense: " << defense;
+        std::cout << "Name: " << name << ", HP: " << health 
+                  << ", Attack: " << attackPower 
+                  << ", Defense: " << defense;
     }
 
-    // Методы для доступа к protected полям
+    
     std::string getName() const { return name; }
     int getHealth() const { return health; }
     int getAttackPower() const { return attackPower; }
@@ -57,18 +55,17 @@ public:
 
 class Character : public Entity {
 private:
-    int criticalChance; // Шанс критического удара (%)
+    int criticalChance; 
 
 public:
     Character(const std::string& n, int h, int a, int d, int crit)
-        : Entity(n, h, a, d), criticalChance(crit) {
-    }
+        : Entity(n, h, a, d), criticalChance(crit) {}
 
-    // Переопределение метода атаки с возможностью критического удара
+    
     void attack(Entity& target) override {
         bool isCritical = (rand() % 100) < criticalChance;
         int damage = attackPower - target.getDefense();
-
+        
         if (isCritical) {
             damage *= 2;
             std::cout << "CRITICAL HIT! ";
@@ -76,23 +73,22 @@ public:
 
         if (damage > 0) {
             target.takeDamage(damage);
-            std::cout << name << " attacks " << target.getName()
-                << " for " << damage << " damage!" << std::endl;
-        }
-        else {
-            std::cout << name << "'s attack has no effect on "
-                << target.getName() << "!" << std::endl;
+            std::cout << name << " attacks " << target.getName() 
+                      << " for " << damage << " damage!" << std::endl;
+        } else {
+            std::cout << name << "'s attack has no effect on " 
+                      << target.getName() << "!" << std::endl;
         }
     }
 
-    // Переопределение метода лечения
+    
     void heal(int amount) override {
         int maxHealth = health + amount > 100 ? 100 : health + amount;
         std::cout << name << " uses healing potion! ";
         Entity::heal(maxHealth - health);
     }
 
-    // Переопределение метода отображения информации
+    
     void displayInfo() const override {
         Entity::displayInfo();
         std::cout << ", Critical: " << criticalChance << "%" << std::endl;
@@ -105,16 +101,15 @@ private:
 
 public:
     Monster(const std::string& n, int h, int a, int d, const std::string& t)
-        : Entity(n, h, a, d), monsterType(t) {
-    }
+        : Entity(n, h, a, d), monsterType(t) {}
 
-    // Переопределение метода атаки
+    
     void attack(Entity& target) override {
         std::cout << monsterType << " ";
         Entity::attack(target);
     }
 
-    // Переопределение метода отображения информации
+    
     void displayInfo() const override {
         Entity::displayInfo();
         std::cout << ", Type: " << monsterType << std::endl;
@@ -126,28 +121,26 @@ private:
     std::string specialAbility;
 
 public:
-    Boss(const std::string& n, int h, int a, int d,
-        const std::string& t, const std::string& ability)
-        : Monster(n, h, a, d, t), specialAbility(ability) {
-    }
+    Boss(const std::string& n, int h, int a, int d, 
+         const std::string& t, const std::string& ability)
+        : Monster(n, h, a, d, t), specialAbility(ability) {}
 
-    // Переопределение метода атаки с использованием способности
+    
     void attack(Entity& target) override {
         std::cout << name << " uses " << specialAbility << "! ";
         int damage = (attackPower * 2) - target.getDefense();
-
+        
         if (damage > 0) {
             target.takeDamage(damage);
-            std::cout << name << " attacks " << target.getName()
-                << " for " << damage << " damage!" << std::endl;
-        }
-        else {
-            std::cout << name << "'s attack has no effect on "
-                << target.getName() << "!" << std::endl;
+            std::cout << name << " attacks " << target.getName() 
+                      << " for " << damage << " damage!" << std::endl;
+        } else {
+            std::cout << name << "'s attack has no effect on " 
+                      << target.getName() << "!" << std::endl;
         }
     }
 
-    // Переопределение метода отображения информации
+    
     void displayInfo() const override {
         Monster::displayInfo();
         std::cout << "Special Ability: " << specialAbility << std::endl;
@@ -155,31 +148,31 @@ public:
 };
 
 int main() {
-    srand(time(0)); // Инициализация генератора случайных чисел
+    srand(time(0)); 
 
-    // Создаем объекты разных типов
-    Character hero("Hero", 100, 25, 10, 20); // 20% шанс крита
+    
+    Character hero("Hero", 100, 25, 10, 20); 
     Monster goblin("Goblin", 50, 15, 5, "Goblin");
     Boss dragon("Dragon", 200, 30, 20, "Ancient Dragon", "Fire Breath");
 
-    // Создаем массив указателей на базовый класс
+    
     std::vector<std::unique_ptr<Entity>> entities;
     entities.push_back(std::make_unique<Character>(hero));
     entities.push_back(std::make_unique<Monster>(goblin));
     entities.push_back(std::make_unique<Boss>(dragon));
 
-    // Демонстрация полиморфизма
+    
     std::cout << "=== Initial Status ===" << std::endl;
     for (const auto& entity : entities) {
         entity->displayInfo();
     }
 
     std::cout << "\n=== Combat ===" << std::endl;
-    // Герой атакует голбина
+    
     entities[0]->attack(*entities[1]);
-    // Гоблин атакует героя
+    
     entities[1]->attack(*entities[0]);
-    // Дракон атакует героя
+    
     entities[2]->attack(*entities[0]);
 
     std::cout << "\n=== After Combat ===" << std::endl;
@@ -188,7 +181,7 @@ int main() {
     }
 
     std::cout << "\n=== Healing ===" << std::endl;
-    // Герой лечится
+    
     entities[0]->heal(30);
 
     std::cout << "\n=== Final Status ===" << std::endl;
--- Lab_2.cpp
+++ Lab_2.cpp
@@ -9,18 +9,18 @@ private:
     float weight;
 
 public:
-    // Конструктор
+    
     Weapon(const std::string& n, int d, float w)
         : name(n), damage(d), weight(w) {
         std::cout << "Weapon " << name << " created!\n";
     }
 
-    // Деструктор
+    
     ~Weapon() {
         std::cout << "Weapon " << name << " destroyed!\n";
     }
 
-    // Метод для отображения информации
+    
     void displayInfo() const {
         std::cout << "Weapon: " << name << ", Damage: " << damage
             << ", Weight: " << weight << " kg\n";
@@ -33,16 +33,16 @@ private:
     int health;
     int attack;
     int defense;
-    Weapon* weapon; // Указатель на оружие
+    Weapon* weapon; 
 
 public:
-    // Конструктор
+    
     Character(const std::string& n, int h, int a, int d, Weapon* w = nullptr)
         : name(n), health(h), attack(a), defense(d), weapon(w) {
         std::cout << "Character " << name << " created!\n";
     }
 
-    // Деструктор
+    
     ~Character() {
         std::cout << "Character " << name << " destroyed!\n";
         if (weapon) {
@@ -50,7 +50,7 @@ public:
         }
     }
 
-    // Метод для установки оружия
+    
     void equipWeapon(Weapon* w) {
         if (weapon) {
             delete weapon;
@@ -80,13 +80,13 @@ private:
     std::string type;
 
 public:
-    // Конструктор
+    
     Monster(const std::string& n, int h, int a, int d, const std::string& t)
         : name(n), health(h), attack(a), defense(d), type(t) {
         std::cout << "Monster " << name << " (" << type << ") created!\n";
     }
 
-    // Деструктор
+    
     ~Monster() {
         std::cout << "Monster " << name << " (" << type << ") destroyed!\n";
     }
@@ -99,19 +99,19 @@ public:
 };
 
 int main() {
-    // Создаем оружие
+    
     std::cout << "=== Creating weapons ===\n";
     Weapon* sword = new Weapon("Steel Sword", 15, 3.2);
     Weapon* axe = new Weapon("Battle Axe", 20, 5.1);
     Weapon* bow = new Weapon("Long Bow", 12, 1.8);
 
-    // Демонстрация работы оружия
+    
     std::cout << "\n=== Weapons info ===\n";
     sword->displayInfo();
     axe->displayInfo();
     bow->displayInfo();
 
-    // Создаем персонажей и монстров
+    
     std::cout << "\n=== Creating characters ===\n";
     Character hero("Hero", 100, 25, 10, sword);
     Character archer("Archer", 80, 20, 8, bow);
@@ -120,7 +120,7 @@ int main() {
     Monster goblin("Goblin", 50, 15, 5, "Goblin");
     Monster orc("Orc", 80, 25, 12, "Orc");
 
-    // Отображаем информацию
+    
     std::cout << "\n=== Characters info ===\n";
     hero.displayInfo();
     archer.displayInfo();
@@ -129,12 +129,12 @@ int main() {
     goblin.displayInfo();
     orc.displayInfo();
 
-    // Меняем оружие
+    
     std::cout << "\n=== Changing weapon ===\n";
     archer.equipWeapon(axe);
     archer.displayInfo();
 
-    // Объекты будут автоматически уничтожены при выходе из области видимости
+    
     std::cout << "\n=== End of scope - objects will be destroyed ===\n";
 
     return 0;
